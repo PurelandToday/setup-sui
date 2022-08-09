c56d60a48f3845d49c3c2fb63a60a68a49338c70
PurelandToday <44767038+PurelandToday@users.noreply.github.com>
2022-08-09 11:08:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #! usr/bin/bash
 echo "Hello World"
+apt-get update \
+    && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends \
+    tzdata \
+    git \
+    ca-certificates \
+    curl \
+    build-essential \
+    libssl-dev \
+    pkg-config \
+    libclang-dev \
+    cmake
