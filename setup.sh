5a364b2561a817a42399ca8c4012d93263e6d8d4
PurelandToday <44767038+PurelandToday@users.noreply.github.com>
2022-08-09 19:00:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ apt-get update \
     libssl-dev \
     pkg-config \
     libclang-dev \
-    cmake
+    cmake \
+    cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 apt-get install -y screen
