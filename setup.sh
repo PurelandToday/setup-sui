18a0a538cdd0262e1f1a4db6469a5e5496814f6f
PurelandToday <44767038+PurelandToday@users.noreply.github.com>
2022-08-09 11:17:03 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,11 @@ apt-get update \
     pkg-config \
     libclang-dev \
     cmake
+rm -r sui
+git clone https://github.com/PurelandToday/sui.git
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout --track upstream/devnet
+cp crates/sui-config/data/fullnode-template.yaml fullnode.yaml
+curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
