25b0677ad265cb75004862778528dc8907f001cc
PurelandToday <44767038+PurelandToday@users.noreply.github.com>
2022-08-14 20:46:37 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! usr/bin/bash
-apt-get update \
-    && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends \
+sudo apt-get update \
+    && sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends \
     tzdata \
     git \
     ca-certificates \
@@ -11,17 +11,16 @@ apt-get update \
     libclang-dev \
     cmake \
     cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-rustup update stable
-apt-get install -y screen
-rm -r sui
-git clone https://github.com/PurelandToday/sui.git
-cd sui
-git remote add upstream https://github.com/MystenLabs/sui
-git fetch upstream
-git checkout --track upstream/devnet
-cp crates/sui-config/data/fullnode-template.yaml fullnode.yaml
-curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-cd sui
-cargo run --release --bin sui-node -- --config-path fullnode.yaml
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+sudo source $HOME/.cargo/env
+sudo rustup update stable
+sudo apt-get install -y screen
+sudo rm -r sui
+sudo git clone https://github.com/PurelandToday/sui.git
+sudo cd sui
+sudo git remote add upstream https://github.com/MystenLabs/sui
+sudo git fetch upstream
+sudo git checkout --track upstream/devnet
+sudo cp crates/sui-config/data/fullnode-template.yaml fullnode.yaml
+sudo curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+sudo cargo run --release --bin sui-node -- --config-path fullnode.yaml
